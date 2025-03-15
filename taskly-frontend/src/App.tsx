e7f77d9fa970b1d9d86c9a15f332510c1a5f3790
LefterisDt <32487066+LefterisD@users.noreply.github.com>
2025-03-15 17:27:15 +0200

    added react router and basic routes

--- taskly-frontend/src/App.tsx
+++ taskly-frontend/src/App.tsx
@@ -1,5 +1,16 @@
+import { Routes, Route } from 'react-router';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import About from './pages/About';
+
 function App() {
-  return <h1>Taskly</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ taskly-frontend/src/pages/Home.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <h1>Taskly !!!!</h1>;
+};
+
+export default Home;
