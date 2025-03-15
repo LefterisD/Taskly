73c6108636116054c582c44162705bf34e04e703
LefterisDt <32487066+LefterisD@users.noreply.github.com>
2025-03-15 20:00:02 +0200

    added todo state

--- taskly-frontend/src/components/TodoInput.tsx
+++ taskly-frontend/src/components/TodoInput.tsx
@@ -5,19 +5,53 @@ import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { useState } from 'react';
 import { motion } from 'framer-motion';
+import dayjs, { Dayjs } from 'dayjs';
+
+type Todo = {
+  text: string;
+  color: string;
+  date: Date | null;
+};
 
 function TodoInput() {
   const theme = useTheme();
+
+  const COLOR_PALETTE: string[] = Object.values(theme.customColors);
+
   const [expanded, setExpanded] = useState<boolean>(false);
-  const [color, setColor] = useState<string>('#dd0000');
+  const [todo, setTodo] = useState<Todo>({
+    text: '',
+    color: '',
+    date: new Date(),
+  });
 
   const handleExpandedClick = () => {
     setExpanded(!expanded);
   };
 
   const handleColorChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setColor(event.target.value);
-    console.log(event.target.value);
+    setTodo((prevState: Todo) => ({
+      ...prevState,
+      color: event.target.value,
+    }));
+  };
+
+  const handleDateChange = (value: Dayjs | null) => {
+    setTodo((prevState: Todo) => ({
+      ...prevState,
+      date: value ? value.toDate() : null,
+    }));
+  };
+
+  const handleTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTodo((prevState: Todo) => ({
+      ...prevState,
+      text: event.target.value,
+    }));
+  };
+
+  const handleSaveTodo = () => {
+    console.log(todo);
   };
 
   return (
@@ -32,6 +66,8 @@ function TodoInput() {
       <Box sx={{ display: 'flex', gap: 1 }}>
         <TextField
           id="outlined-basic"
+          value={todo.text}
+          onChange={handleTextChange}
           variant="standard"
           placeholder="Add a new todo"
           sx={{ flexGrow: '1' }}
@@ -39,6 +75,7 @@ function TodoInput() {
         <Button
           variant="contained"
           startIcon={<AddIcon />}
+          onClick={handleSaveTodo}
           sx={{ backgroundColor: theme.customColors.orange }}
         >
           Add
@@ -61,24 +98,28 @@ function TodoInput() {
         }
         exit={{ height: 0, opacity: 0 }}
         transition={{ duration: 0.2, ease: 'easeInOut' }}
-        style={{ overflow: 'hidden' }} // Prevents abrupt jumps
+        style={{ overflow: 'hidden' }}
       >
         <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
           <Box sx={{ display: 'flex', gap: 1 }}>
             <Typography sx={{ minWidth: '9rem' }}>Select color:</Typography>
-            <input type="color" value={color} onChange={handleColorChange} />
+            <input
+              type="color"
+              value={todo.color}
+              onChange={handleColorChange}
+            />
           </Box>
           <Box sx={{ display: 'flex', gap: 1, alignItems: 'center' }}>
             <Typography sx={{ minWidth: '9rem' }}>
               Select starting from:
             </Typography>
-            <DatePicker />
+            <DatePicker
+              value={todo.date ? dayjs(todo.date) : null}
+              onChange={handleDateChange}
+            />
           </Box>
         </Box>
       </motion.div>
-      {/* {expanded && (
-        
-      )} */}
     </Box>
   );
 }
