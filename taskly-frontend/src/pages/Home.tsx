ce075f277f707f549d54ac4a9e7075451789fde8
LefterisDt <32487066+LefterisD@users.noreply.github.com>
2025-03-15 21:46:26 +0200

    fixed centering issue with todos

--- taskly-frontend/src/pages/Home.tsx
+++ taskly-frontend/src/pages/Home.tsx
@@ -24,14 +24,15 @@ const Home = () => {
       justifyContent="center"
       alignItems="center"
       direction="column"
+      size={{ xs: 12, sm: 8, md: 6 }}
     >
-      <Grid size={{ xs: 12, sm: 8, md: 6 }} sx={{ mb: 4 }}>
+      <Grid size={{ xs: 12, sm: 8, md: 6 }} sx={{ mb: 4, maxWidth: '700px' }}>
         <TodoInput />
       </Grid>
       {todos.length !== 0 &&
         todos.map((todo: TodoType) => {
           return (
-            <Grid size={{ xs: 12, sm: 8, md: 6 }}>
+            <Grid size={{ xs: 12, sm: 8, md: 6 }} sx={{ maxWidth: '700px' }}>
               <Todo todo={todo} />
             </Grid>
           );
