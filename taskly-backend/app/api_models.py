801b6035bcdcb94e1df7e278722dea7f208c09b5
LefterisDt <32487066+LefterisD@users.noreply.github.com>
2025-03-16 16:29:10 +0200

    added more columns

--- taskly-backend/app/api_models.py
+++ taskly-backend/app/api_models.py
@@ -7,12 +7,24 @@ todo_model = api.model("Todo", {
     "id": fields.Integer,
     "name": fields.String,
     "color": fields.String,
-    "created": fields.DateTime
+    "created": fields.DateTime,
+    "hours": fields.Float,
+    "completed": fields.Boolean,
+    "completed_at": fields.DateTime
 })
 
+
+
+todo_input_model_post = api.model("TodoInputPost", {
+    "name": fields.String,
+    "color": fields.String,
+    "created": fields.DateTime,
+})
 
 todo_input_model = api.model("TodoInput", {
     "name": fields.String,
     "color": fields.String,
-    "created": fields.DateTime
+    "created": fields.DateTime,
+    "completed": fields.Boolean,
+    "completed_at": fields.DateTime
 })
