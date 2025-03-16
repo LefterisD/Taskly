0232b27c12f108d545437d04522c653b740d5a49
LefterisDt <32487066+LefterisD@users.noreply.github.com>
2025-03-16 17:28:09 +0200

    added flask cors

--- taskly-backend/app/__init__.py
+++ taskly-backend/app/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask 
-
+from flask_cors import CORS 
 from .extensions import api, db
 from .resources import ns
 
 def create_app():
     app = Flask(__name__)
 
+    CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
 
     api.init_app(app)
