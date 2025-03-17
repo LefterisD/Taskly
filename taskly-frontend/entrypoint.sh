91aa8ce6cc49a360a284c40501bff04e296d10e8
LefterisDt <32487066+LefterisD@users.noreply.github.com>
2025-03-17 09:34:25 +0200

    API_BASE_URL

--- taskly-frontend/entrypoint.sh
+++ taskly-frontend/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Generate .env file dynamically at runtime
-echo "VITE_API_URL=${VITE_API_URL}" > /app/.env
+echo "VITE_API_URL=${VITE_API_BASE_URL}" > /app/.env
 
 # Start the app
 exec "$@"
