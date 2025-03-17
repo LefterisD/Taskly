a5a5bcfc8887efb7ed8d14dfdf19f72fc81b70a6
LefterisDt <32487066+LefterisD@users.noreply.github.com>
2025-03-17 09:54:02 +0200

    fixed wrong env name

--- taskly-frontend/src/api/index.tsx
+++ taskly-frontend/src/api/index.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_BASE_URL = import.meta.env.VITE_API_URL;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const instance = axios.create({
   baseURL: API_BASE_URL,
